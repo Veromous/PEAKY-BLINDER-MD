5269a74c6eeffe5fac79085d0a58452850db9f36
Veromous <140233943+Veromous@users.noreply.github.com>
2025-11-06 21:31:30 +0100

    Update settings.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~cMQ2iJAS#gU9FuF6MeD2WXpwJF5gon4ODhjC0O5OI4b9fDnx_yFo",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~lVISBTrY#PlYZ82We6uHhUfY5k-laBUYk2-jffh1ng53fHtgdE34",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
